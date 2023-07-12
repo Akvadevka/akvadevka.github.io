a9fb35e3c29699ca403b7658bc9b1db76790958c
Akvadevka <ksushechkavo@gmail.com>
2023-07-12 17:14:46 +0300

    assignment fix img

--- src/routes/Button1.svelte
+++ src/routes/Button1.svelte
@@ -1,6 +1,10 @@
+<script lang="ts">
+	let comic = './comic/' as const;
+</script>
+
 <div class="joke">
 	<h3>I WANNA JOKE</h3>
-	<form action="./comic" target="_blank">
+	<form action={comic} target="_blank">
 		<button class="btn_joke">See the joke</button>
 	</form>
 </div>
