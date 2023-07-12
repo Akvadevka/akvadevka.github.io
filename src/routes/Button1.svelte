dd4e0fcc72b0c87201b34c5ab659b4c5d870dcd1
Akvadevka <ksushechkavo@gmail.com>
2023-07-12 17:06:01 +0300

    assignment fix img

--- src/routes/Button1.svelte
+++ src/routes/Button1.svelte
@@ -1,10 +1,6 @@
-<script>
-	import comic from './comic/+page.svelte';
-</script>
-
 <div class="joke">
 	<h3>I WANNA JOKE</h3>
-	<form action={comic} target="_blank">
+	<form action="./comic" target="_blank">
 		<button class="btn_joke">See the joke</button>
 	</form>
 </div>
